9a2f20ff3fca54bdc090314d139357ffbd8d331d
ActivePeter <1020401660@qq.com>
2024-04-16 16:34:13 +0000

    feat: sofaboot source code

--- scripts/1.prepare.py
+++ scripts/1.prepare.py
@@ -133,6 +133,9 @@ def prepare_sofa_registy():
 prepare_sofa_registy()
 
 
+def prepare_sofa_boot():
+    os_system_sure("python3 1.1prepare_sofaboot.py")
+
 ### prepare demo prj
 def prepare_demo_prj():
     if not os.path.exists("../kc-sofastack-demo"):
